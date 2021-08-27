aa25364eff8d81fd34338f594906359bde1d18ad
Nuds94 <87452709+Nuds94@users.noreply.github.com>
2021-08-27 14:13:05 +1000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="app">
+<header>
+<dropdown />
+</header>
+
+</div>
+
+
+</template>
+
+<script>
+import dropdown from "./components/dropdown.vue"
+
+
+
+export default {
+  name: 'App',
+  components: {
+    dropdown
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/cookschoice.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class='carousel-view'>
+    <transition-group
+      class='carousel'
+      tag="div">
+      <div
+        v-for="slide in slides" 
+        class='slide'
+        :key="slide.id">
+        <h4> {{ slide.title }} </h4>
+      </div>
+    </transition-group>
+    <div class='carousel-controls'>
+      <button class='carousel-controls__button' @click="previous">prev</button>
+      <button class='carousel-controls__button' @click="next">next</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      slides: [
+        {
+          title: 'Spring Rolls',
+          id: 1
+        },
+        {
+          title: 'Spaghetti Bolagnase',
+          id: 2
+        },
+        {
+          title: 'Chocolate Cake',
+          id: 3
+        },
+        {
+          title: 'Orange marmalade',
+          id: 4
+        },
+      ]
+    }
+  },
+  methods: {
+    next () {
+      const first = this.slides.shift()
+      this.slides = this.slides.concat(first)
+    },
+    previous () {
+      const last = this.slides.pop()
+      this.slides = [last].concat(this.slides)
+    }
+  }
+}
+</script>
+
+<style>
+.carousel-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.carousel {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  
+  width: 24em;
+  min-height: 25em;
+}
+.slide {
+  flex: 0 0 20em;
+  height: 20em;
+  margin: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0.1em dashed #000;
+  border-radius: 50%;
+  transition: transform 0.3s ease-in-out;
+}
+.slide:first-of-type {
+  opacity: 0;
+}
+.slide:last-of-type {
+  opacity: 0;
+}
+</style>
